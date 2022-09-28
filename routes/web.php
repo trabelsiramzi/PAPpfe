86fac08c1fab75fdc04c0a94264f017fb0471892
trabelsiramzi <trabelsiramzi.tr@gmail.com>
2022-09-28 13:53:42 +0100

    modif root

--- routes/web.php
+++ routes/web.php
@@ -113,7 +113,7 @@ use Illuminate\Support\Facades\Route;
     });
 
     Auth::routes();
-
+ 
     Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
     Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 //---------------------------MANAGER-----------------------------
